79adc00a64363174a8568cdd25ed5749c74756d7
discordedirneproject <ugandabuganda08@gmail.com>
2025-11-23 15:35:11 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+});
+
+client.once(Events.ClientReady, readyClient => {
+    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.on('messageCreate', message => {
+    if(message.content === "!ping") message.reply("Pong!");
+});
+
+client.login(process.env.TOKEN).catch(err => console.error("Token hatalı veya bot ekli değil:", err));
